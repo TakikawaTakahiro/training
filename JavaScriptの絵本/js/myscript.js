d16cc676e0d4443374ff503b02bb521c1514a3e5
滝川 貴大 <takikawa@rightcode.co.jp>
2015-07-21 15:43:44 +0900

    JavaScriptの絵本 ３章 変数、演算子

--- JavaScriptの絵本/js/myscript.js
+++ JavaScriptの絵本/js/myscript.js
@@ -1,5 +1,19 @@
-document.write("myscriptの呼び出しを確認"); 　//window.は省略可能
+document.write("myscriptの呼び出しを確認", "<br>"); 　//window.は省略可能
 // alert("thank you!"); // こちらも省略可能なので省略済み
-document.fgColor = "white";
+document.fgColor = "blue";
 document.bgColor = "pink";
-window.resizeTo(600,300);
+
+a = typeof(123);
+b = typeof("hallo world");
+c = typeof(true);
+d = typeof(window);
+e = typeof(null);
+f = typeof(undefined);
+document.write(a,"<br>",b,"<br>",c,"<br>",d,"<br>",e,"<br>",f,"<br>");
+
+
+z = 16;
+(z < 10) || (document.write("zは１０以上である","<br>"));
+(z < 10) && (document.write("zは１０未満である","<br>"));
+
+document.write("2x8+3÷(5-2)x3=", (2*8+3/(5-2)*3),"<br>");
